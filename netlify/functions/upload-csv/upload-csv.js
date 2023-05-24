19321a74b74ad5295aabb7d97bb7c026053a0ec7
Vinay Vinod <briarvinay@gmail.com>
2023-05-24 14:25:11 -0700

    push

--- netlify/functions/upload-csv/upload-csv.js
+++ netlify/functions/upload-csv/upload-csv.js
@@ -1,46 +1,39 @@
-const fetch = require('isomorphic-fetch');
+const AWS = require('aws-sdk');
 
 exports.handler = async function(event, context, callback) {
-  const accessToken = process.env.api;
-  const uploadUrl = 'https://content.dropboxapi.com/2/files/upload';
-  const filePath = `/participant_choices_${Date.now()}.csv`;
-
   const csvContent = event.body; // The CSV content received from the frontend
-  const headers = {
-    'Authorization': 'Bearer ' + accessToken,
-    'Content-Type': 'application/octet-stream',
-    'Dropbox-API-Arg': JSON.stringify({
-      path: filePath,
-      mode: 'add',
-      autorename: true,
-      mute: false
-    })
+  
+  // Configure AWS SDK with your credentials and the desired endpoint URL
+  AWS.config.update({
+    accessKeyId: process.env.key_id,
+    secretAccessKey: process.env.secret_key,
+    region: process.env.region,
+    s3ForcePathStyle: true,
+    endpoint: process.env.endpoint, 
+  });
+
+  const s3 = new AWS.S3();
+
+  const bucketName = process.env.AWS_BUCKET_NAME;
+  const timestamp = new Date().toISOString();
+  const filePath = `participant_choices_${timestamp}.csv`;
+
+  const params = {
+    Bucket: bucketName,
+    Key: filePath,
+    Body: csvContent,
   };
 
   try {
-    const response = await fetch(uploadUrl, {
-      method: 'POST',
-      headers: headers,
-      body: csvContent
-    });
-
-    if (response.ok) {
-      const result = await response.json();
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'File uploaded successfully', result })
-      };
-    } else {
-      const error = await response.text();
-      return {
-        statusCode: response.status,
-        body: JSON.stringify({ error })
-      };
-    }
+    const response = await s3.upload(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'File uploaded successfully', response }),
+    };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred during file upload' })
+      body: JSON.stringify({ error: 'An error occurred during file upload' }),
     };
   }
 };
