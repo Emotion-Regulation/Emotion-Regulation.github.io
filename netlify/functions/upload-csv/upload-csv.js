7968cb30e74c9072be99eafe09c7888a06f865aa
Vinay Vinod <briarvinay@gmail.com>
2023-05-24 11:33:58 -0700

    push

--- netlify/functions/upload-csv/upload-csv.js
+++ netlify/functions/upload-csv/upload-csv.js
@@ -1,11 +1,38 @@
 const fetch = require('isomorphic-fetch');
 
 exports.handler = async function(event, context, callback) {
-  const accessToken = process.env.api;
+  const clientId = process.env.dropboxClientId;
+  const clientSecret = process.env.dropboxClientSecret;
+  const refreshToken = process.env.api;
   const uploadUrl = 'https://content.dropboxapi.com/2/files/upload';
   const filePath = `/participant_choices_${Date.now()}.csv`;
 
+  // Helper function to refresh the access token
+  const refreshAccessToken = async () => {
+    const authUrl = 'https://api.dropbox.com/oauth2/token';
+    const body = new URLSearchParams();
+    body.append('grant_type', 'refresh_token');
+    body.append('refresh_token', refreshToken);
+    body.append('client_id', clientId);
+    body.append('client_secret', clientSecret);
+
+    const response = await fetch(authUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: body.toString()
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.access_token;
+    } else {
+      throw new Error('Failed to refresh access token');
+    }
+  };
+
   const csvContent = event.body; // The CSV content received from the frontend
+  const accessToken = await refreshAccessToken(); // Obtain a new access token
+
   const headers = {
     'Authorization': 'Bearer ' + accessToken,
     'Content-Type': 'application/octet-stream',
